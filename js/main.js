26cdc1addd51bf9777a2f1057171cbd22bff642e
dunitrashuk <mamaligadumitras@gmail.com>
2023-09-04 17:44:32 +0300

    featured section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import selectWhite from "./components/selectWhite";
 import heroCards from "./components/heroCards";
+import featuredCards from "./components/featuredCards";
 
 selectWhite();
 heroCards();
+featuredCards();
