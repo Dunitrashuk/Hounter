a0f227d0572873189b94102f9bcf8159f9d1ac5d
dunitrashuk <mamaligadumitras@gmail.com>
2023-09-05 14:45:15 +0300

    tour section

---
+++ js/components/videoModal.js
@@ -0,0 +1,18 @@
+export default function videoModal() {
+  //selectors
+  const videoButton = document.querySelector(
+    ".tour__content__images__main-img"
+  );
+  const videoModal = document.querySelector(".video-modal");
+
+  //events
+  videoButton.addEventListener("click", (e) => {
+    videoModal.classList.add("open-modal");
+    document.querySelector("html").style.overflow = "hidden";
+  });
+
+  videoModal.addEventListener("click", (e) => {
+    videoModal.classList.remove("open-modal");
+    document.querySelector("html").style.overflow = "visible";
+  });
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import selectWhite from "./components/selectWhite";
 import heroCards from "./components/heroCards";
 import featuredCards from "./components/featuredCards";
+import videoModal from "./components/videoModal";
 
 selectWhite();
 heroCards();
 featuredCards();
+videoModal();
